5cbf083503f1c45924a797f72d2f671f97de45da
Jeroen Ouwehand <jeroenouw@outlook.com>
2019-06-04 15:08:07 +0200

    Added all options to cgx and index files

--- src/cgx.ts
+++ src/cgx.ts
@@ -5,13 +5,21 @@ import { Logger } from './utils/logger';
 import { CodeOfConduct } from './options/code-of-conduct';
 import { License } from './options/license';
 import { Choice, ChoiceValue, Answer } from './models/choice';
+import { Contributing } from './options/contributing';
+import { BugReport } from './options/bug-report';
+import { FeatureRequest } from './options/feature-request';
+import { PullRequestTemplate } from './options/pull-request-template';
 
 @injectable()
 export class CGX {
 
     constructor(@inject('Logger') private logger: Logger,
                 @inject('CodeOfConduct') private codeOfConduct: CodeOfConduct,
-                @inject('License') private license: License) {
+                @inject('License') private license: License,
+                @inject('Contributing') private contributing: Contributing,
+                @inject('BugReport') private bugReport: BugReport,
+                @inject('FeatureRequest') private featureRequest: FeatureRequest,
+                @inject('PullRequestTemplate') private pullRequestTemplate: PullRequestTemplate) {
         this.logger.showBanner();
         this.executeCGX();
     }
@@ -24,26 +32,41 @@ export class CGX {
                 this.logger.showInfo('Start generating all recommended files...');
 
                 this.codeOfConduct.generateCodeOfConduct();
-                this.license.generateLicense();
-            }
-            case ChoiceValue.CODE_OF_CONDUCT: {
-                this.codeOfConduct.generateCodeOfConduct();
+                this.contributing.generateContributing();
+                this.bugReport.generateBugReport();
+                this.featureRequest.generateFeatureRequest();
+                return this.pullRequestTemplate.generatePullRequestTemplate();
             }
             case ChoiceValue.LICENSE: {
-                this.license.generateLicense();
+                return this.license.generateLicense();
+            }
+            case ChoiceValue.CODE_OF_CONDUCT: {
+                return this.codeOfConduct.generateCodeOfConduct();
+            }
+            case ChoiceValue.CONTRIBUTING: {
+                return this.contributing.generateContributing();
+            }
+            case ChoiceValue.BUG_REPORT: {
+                return this.bugReport.generateBugReport();
+            }
+            case ChoiceValue.FEATURE_REQUEST: {
+                return this.featureRequest.generateFeatureRequest();
+            }
+            case ChoiceValue.PULL_REQUEST_TEMPLATE: {
+                return this.pullRequestTemplate.generatePullRequestTemplate();
             }
         }
     }
 
     private async askFileQuestion(): Promise<any> {
         const listOfFiles: Choice[] = [
-            {name: 'License', value: ChoiceValue.LICENSE},
+            {name: 'All files (without license)', value: ChoiceValue.ALL},
+            {name: 'License (MIT)', value: ChoiceValue.LICENSE},
             {name: 'Code of conduct', value: ChoiceValue.CODE_OF_CONDUCT},
-            {name: 'Coming soon - All files', value: ChoiceValue.ALL},
-            {name: 'Coming soon - Contributing', value: ChoiceValue.CONTRIBUTING},
-            {name: 'Coming soon - Bug report (issue)', value: ChoiceValue.BUG_REPORT},
-            {name: 'Coming soon - Feature request (issue)', value: ChoiceValue.FEATURE_REQUEST},
-            {name: 'Coming soon - Pull request template', value: ChoiceValue.PULL_REQUEST_TEMPLATE},
+            {name: 'Contributing', value: ChoiceValue.CONTRIBUTING},
+            {name: 'Bug report (issue)', value: ChoiceValue.BUG_REPORT},
+            {name: 'Feature request (issue)', value: ChoiceValue.FEATURE_REQUEST},
+            {name: 'Pull request template', value: ChoiceValue.PULL_REQUEST_TEMPLATE},
         ];
 
         return inquirer.prompt([{ 
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ import { Logger } from './utils/logger';
 import { CodeOfConduct } from './options/code-of-conduct';
 import { License } from './options/license';
 import { Checker } from './utils/checker';
+import { Contributing } from './options/contributing';
+import { BugReport } from './options/bug-report';
+import { FeatureRequest } from './options/feature-request';
+import { PullRequestTemplate } from './options/pull-request-template';
 
 export function index(): CGX {
   const container: Container = new Container();
@@ -16,6 +20,10 @@ export function index(): CGX {
   // Options
   container.bind<CodeOfConduct>('CodeOfConduct').to(CodeOfConduct).inSingletonScope();
   container.bind<License>('License').to(License).inSingletonScope();
+  container.bind<Contributing>('Contributing').to(Contributing).inSingletonScope();
+  container.bind<BugReport>('BugReport').to(BugReport).inSingletonScope();
+  container.bind<FeatureRequest>('FeatureRequest').to(FeatureRequest).inSingletonScope();
+  container.bind<PullRequestTemplate>('PullRequestTemplate').to(PullRequestTemplate).inSingletonScope();
 
   // CGX
   container.bind<CGX>('CGX').to(CGX).inSingletonScope();
