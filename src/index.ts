cff03243435915682b209416475a011e59e35369
Jeroen Ouwehand <jeroenouw@outlook.com>
2019-06-07 14:42:55 +0200

    Added gitlab options: bug, ci-template and feature-proposal

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,17 @@ import { Container } from 'inversify';
 import { CGX } from './cgx';
 import { Logger } from './utils/logger';
 import { CodeOfConduct } from './options/github/code-of-conduct';
-import { License } from './options/github/license';
+import { License } from './options/universal/license';
 import { Checker } from './utils/checker';
-import { Contributing } from './options/github/contributing';
+import { Contributing } from './options/universal/contributing';
 import { BugReport } from './options/github/bug-report';
 import { FeatureRequest } from './options/github/feature-request';
 import { PullRequestTemplate } from './options/github/pull-request-template';
 import { DefaultTemplate } from './options/default/default.template';
 import { MergeRequestTemplate } from './options/gitlab/merge-request-template';
+import { Bug } from './options/gitlab/bug';
+import { CITemplate } from './options/gitlab/ci-template';
+import { FeatureProposal } from './options/gitlab/feature-proposal';
 
 export function index(): CGX {
   const container: Container = new Container();
@@ -22,15 +25,20 @@ export function index(): CGX {
   // Default Template
   container.bind<DefaultTemplate>('DefaultTemplate').to(DefaultTemplate).inSingletonScope();
 
-  // Github Options
-  container.bind<CodeOfConduct>('CodeOfConduct').to(CodeOfConduct).inSingletonScope();
+  // Github & Gitlab Options (universal)
   container.bind<License>('License').to(License).inSingletonScope();
   container.bind<Contributing>('Contributing').to(Contributing).inSingletonScope();
+
+  // Github Options
+  container.bind<CodeOfConduct>('CodeOfConduct').to(CodeOfConduct).inSingletonScope();
   container.bind<BugReport>('BugReport').to(BugReport).inSingletonScope();
   container.bind<FeatureRequest>('FeatureRequest').to(FeatureRequest).inSingletonScope();
   container.bind<PullRequestTemplate>('PullRequestTemplate').to(PullRequestTemplate).inSingletonScope();
 
   // Gitlab Options
+  container.bind<CITemplate>('CITemplate').to(CITemplate).inSingletonScope();
+  container.bind<FeatureProposal>('FeatureProposal').to(FeatureProposal).inSingletonScope();
+  container.bind<Bug>('Bug').to(Bug).inSingletonScope();
   container.bind<MergeRequestTemplate>('MergeRequestTemplate').to(MergeRequestTemplate).inSingletonScope();
 
   // CGX
