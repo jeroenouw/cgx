b01d5586144b4a927e8eb9a355f8fe5739f82167
Jeroen Ouwehand <jeroenouw@outlook.com>
2019-06-18 16:26:16 +0200

    Misspelled Gitlab

--- src/models/console-message.ts
+++ src/models/console-message.ts
@@ -1,6 +1,6 @@
 export enum ConsoleMessage {
     TITLE = 'CGX',
-    BANNER = 'Generate all recommended documentation/files for the Github and Gitlub community standards',
+    BANNER = 'Generate all recommended documentation/files for the Github and Gitlab community standards',
     ERROR = 'ERROR: ',
     SUCCESS = 'SUCCESS: ',
     INFO = 'INFO: ',
