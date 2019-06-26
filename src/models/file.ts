040d7279006f9b9c7d6a94b49461d475d06c1d80
Jeroen Ouwehand <jeroenouw@outlook.com>
2019-06-26 20:07:58 +0200

    Added FileName to keep names at one place

---
+++ src/models/file.ts
@@ -0,0 +1,14 @@
+export enum FileName {
+    BUG_REPORT = 'bug_report.md',
+    FEATURE_REQUEST = 'feature_request.md',
+    PULL_REQUEST = 'pull_request_template.md',
+    BUG = 'Bug.md',
+    CI_TEMPLATE = 'example-ci.yml',
+    FEATURE_PROPOSAL = 'Feature proposal.md',
+    MERGE_REQUEST = 'merge_request_template.md',
+    CODE_OF_CONDUCT = 'CODE_OF_CONDUCT.md',
+    CONTRIBUTING = 'CONTRIBUTING.md',
+    LICENSE = 'LICENSE',
+    README = 'README.md',
+    TODO = 'TODO.md',
+}
